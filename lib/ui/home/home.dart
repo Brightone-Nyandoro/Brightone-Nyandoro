7bb41888ccd1e8b32260ea8b213ef67ecfeedf17
PrideFafy <tafadzwa.pmusendo@gmail.com>
2021-05-20 21:52:08 +0200

    added option to display nearby places

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,3 +1,5 @@
+import 'package:cut_school_map/ui/home/places_search_map.dart';
+import 'package:cut_school_map/ui/home/search_filter.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -13,6 +15,8 @@ class _HomeState extends State<Home> {
       CameraPosition(target: _kMapCenter, zoom: 11.0, tilt: 0, bearing: 0);
   GoogleMapController _controller;
   Set<Marker> _markers = {};
+
+  static String keyword = "Library";
 
   static const LatLng _center = const LatLng(-17.349722, 30.194444);
   LatLng _lastMapPosition = _center;
@@ -32,19 +36,40 @@ class _HomeState extends State<Home> {
     _lastMapPosition = position.target;
   }
 
+  void updateKeyWord(String newKeyword) {
+    print(newKeyword);
+    setState(() {
+      keyword = newKeyword;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('CUT Campus Navigation'),
+        actions: <Widget>[
+          Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                  icon: Icon(Icons.filter_list),
+                  tooltip: 'Filter Search',
+                  onPressed: () {
+                    Scaffold.of(context).openEndDrawer();
+                  });
+            },
+          ),
+        ],
       ),
-      body: GoogleMap(
-        initialCameraPosition: _kInitialPosition,
-        onMapCreated: onMapCreated,
-        mapType: MapType.normal,
-        markers: _markers,
-        myLocationEnabled: true,
-      ),
+      body: PlacesSearchMapSample(keyword),
+      endDrawer: SearchFilter(updateKeyWord),
+      // body: GoogleMap(
+      //   initialCameraPosition: _kInitialPosition,
+      //   onMapCreated: onMapCreated,
+      //   mapType: MapType.normal,
+      //   markers: _markers,
+      //   myLocationEnabled: true,
+      // ),
     );
   }
 }
