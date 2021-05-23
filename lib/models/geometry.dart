78ea1e7725d69bbbca9e9ab89f5a4c23deeb647b
PrideFafy <tafadzwa.pmusendo@gmail.com>
2021-05-23 13:16:21 +0200

    new search classes

---
+++ lib/models/geometry.dart
@@ -0,0 +1,10 @@
+import 'package:cut_school_map/models/location.dart';
+
+class Geometry {
+  final Location location;
+
+  Geometry({this.location});
+
+  Geometry.fromJson(Map<dynamic, dynamic> parsedJson)
+      : location = Location.fromJson(parsedJson['location']);
+}
--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -1,5 +1,6 @@
 import 'package:cut_school_map/ui/custom_components/rounded_button.dart';
 import 'package:cut_school_map/ui/home/home.dart';
+import 'package:cut_school_map/ui/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
@@ -32,7 +33,7 @@ class WelcomePage extends StatelessWidget {
                       context,
                       MaterialPageRoute(
                         builder: (context) {
-                          return Home();
+                          return HomeScreen();
                         },
                       ),
                     );
